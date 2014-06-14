f26b57c65483214dcc90c2df57776a075564ccdd
J. Randall Hunt <randallhunt@gmail.com>
2014-06-13 22:19:35 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask.ext.social import Social
+app = Flask(__name__)
+app.config['SOCIAL_FACEBOOK'] = {
+    'consumer_key': '',
+    'consumer_secret': ''
+}
+
+@app.route('/')
+def index():
+    reterun "Hello, World!"
+
+if __name__ == '__main__':
+   app.run()
+
